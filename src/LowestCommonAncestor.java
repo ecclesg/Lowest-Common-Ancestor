9e7628e89b4630dd4b027a1f5b04391fd5aa81bf
Gavin Eccles <ecclesg@tcd.ie>
2017-10-12 16:48:24 +0100

    First Committ

---
+++ src/LowestCommonAncestor.java
@@ -0,0 +1,17 @@
+class Node {
+    int data;
+    Node left, right;
+ 
+    Node(int value) {
+        data = value;
+        left = right = null;
+    }
+}
+
+public class LowestCommonAncestor {
+	Node root;
+	
+	int findLCA(int n1, int n2) {
+		return -1;
+	}
+}
--- src/LowestCommonAncestorTEST.java
+++ src/LowestCommonAncestorTEST.java
@@ -1,4 +1,27 @@
+import org.junit.Test;
 
 public class LowestCommonAncestorTEST {
 
+	@Test
+	public void testForEmptyTree(){
+	
+	}
+	
+	@Test
+	public void testAncestorNode(){
+		
+	}
+	
+	@Test
+	public void testTwoNodes(){
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
 }
