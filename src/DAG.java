201685ad5594d011146ba741a62aa4adf3f72680
Gavin Eccles <ecclesg@tcd.ie>
2017-10-29 10:44:40 +0000

    Implementation of DAG class- addEdge

--- src/DAG.java
+++ src/DAG.java
@@ -17,14 +17,28 @@ public class DAG {
 	//Adds a directed edge from v->w
 	public void addEdge(int v, int w)
 	{
-		
+		validateVertex(v);
+	    validateVertex(w);
+	    adj[v].add(w);
+	    indegree[w]++;
+	    E++;
+
 	}
+	
+	private void validateVertex(int v) {
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+
 	
 	//Returns amount of directed edges incident to current vertex
 	public int indegree(int v) {
-	     return indegree[v];
+		validateVertex(v);
+        return indegree[v];
+
 	}
 		
+	
 	//Returns the adjacent vertices to v
 	public Iterable<Integer> adj(int v)
 	{ return adj[v]; }
