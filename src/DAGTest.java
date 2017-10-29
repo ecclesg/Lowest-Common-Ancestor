90eb97dc4c46a5dcd9ffc4d3e7e4572cbecff509
Gavin Eccles <ecclesg@tcd.ie>
2017-10-29 11:36:48 +0000

    Tests for directed Graph

--- src/DAGTest.java
+++ src/DAGTest.java
@@ -1,12 +1,49 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
 
 import org.junit.Test;
 
 public class DAGTest {
 	
-	//Following test check DAG class works correctly
+	//Following tests check Directed Graph class works correctly
 	@Test
-	public void testForDAG(){
+	public void testForDirectedGraph(){
+		DAG test = new DAG(10);
+		test.addEdge(1, 2);
+		test.addEdge(1, 3);
+		test.addEdge(3, 4);
+		test.addEdge(4, 5);
+		test.addEdge(4, 6);
+		
+		assertEquals("", 1, test.indegree(4));
+		assertEquals("", 2, test.outdegree(4));
+		assertEquals("", 5, test.E());
+		assertEquals("", 10, test.V());
+		String ans = "[5, 6]";
+		assertEquals("",ans, test.adj(4).toString());
+	}
+	
+	@Test(expected=Exception.class)
+	public void exceptionTest(){
+		//Can't make a directed graph with less than 0 vertices
+		DAG test3 = new DAG(-5);
+		
+		//DAG test4 = new DAG(5);
+		//Can't add an edge to a vertices > V(5)
+		//test4.addEdge(3, 9);
+		//test4.addEdge(-2, 3);
+		//test4.addEdge(0, 5);		
+	}
+	
+	
+	
+	
+	
+	//Following test check Directed ACYCLIC Graph class works correctly
+	@Test
+	public void testForDirecedAcyclicGraph(){
+			
 	}
 	
 
