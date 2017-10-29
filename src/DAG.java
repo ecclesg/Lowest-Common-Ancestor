de25a0cf4bfb940652bb598c00fe170ec3f06176
Gavin Eccles <ecclesg@tcd.ie>
2017-10-29 17:06:18 +0000

    1st Attempt to Solve LCA using bfs

--- src/DAG.java
+++ src/DAG.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class DAG {
 	private int V;           // number of vertices in this digraph
@@ -8,6 +11,9 @@ public class DAG {
 	private boolean marked[];		//Boolean List to track visited vertices
 	private boolean hasCycle;		//True if cycle in graph
     private boolean stack[];		//Order that vertices were visited
+    private int[] edgeTo;      // edgeTo[v] = last edge on shortest s->v path
+    private int[] distTo;      // distTo[v] = length of shortest s->v path
+
     
 
 
@@ -23,7 +29,7 @@ public class DAG {
 	    adj = (ArrayList<Integer>[]) new ArrayList[V];
 	    for (int v = 0; v < V; v++) {
 	        adj[v] = new ArrayList<Integer>();
-	    }
+	    }              
 	}
 
 	//Returns current vertex
@@ -87,6 +93,7 @@ public class DAG {
 	
 	
 	public boolean hasCycle() {
+
         return hasCycle;
     }
 	
@@ -107,8 +114,47 @@ public class DAG {
 	        stack[v] = false;
 	    }
 	 
+	public int findLCA(int v, int w){
+		return 0;
+	}
 	
 	
+	// prints BFS traversal from a given source s
+    void BFS(int s)
+    {
+        // Mark all the vertices as not visited(By default
+        // set as false)
+        boolean visited[] = new boolean[V];
+ 
+        // Create a queue for BFS
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+ 
+        // Mark the current node as visited and enqueue it
+        visited[s]=true;
+        queue.add(s);
+ 
+        while (queue.size() != 0)
+        {
+            // Dequeue a vertex from queue and print it
+            s = queue.poll();
+            System.out.print(s+" ");
+ 
+            // Get all adjacent vertices of the dequeued vertex s
+            // If a adjacent has not been visited, then mark it
+            // visited and enqueue it
+            Iterator<Integer> i = adj[s].listIterator();
+            while (i.hasNext())
+            {
+                int n = i.next();
+                if (!visited[n])
+                {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
+    }
+
 	
 
 	
