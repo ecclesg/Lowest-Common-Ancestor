816d0076a8b5860264803b8517071f919609c467
Gavin Eccles <ecclesg@tcd.ie>
2017-10-29 10:18:03 +0000

    Initial Test Outlines before implementation

--- src/DAGTest.java
+++ src/DAGTest.java
@@ -1,7 +1,30 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class DAGTest {
 	
+	//Following test check DAG class works correctly
+	@Test
+	public void testForDAG(){
+	}
 	
+
+	
+	
+	//Following tests check the LCA function for a DAG graph
+	@Test
+	public void testForEmptyTree(){
+		
+	}
+	
+	@Test
+	public void testForTree1Vertex(){
+	}
+	
+	@Test
+	public void testForNonExistingVertices(){
+	}
 	
 	
 }
